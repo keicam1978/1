838e13f5a1f9a5363593f50e7448efc204bb691b
Artur Ciesla <ezechiasz.ofm@gmail.com>
2018-12-05 21:07:58 +0100

    0.02

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-    public static void pierwszy(String[] args){
+    public static void main(String[] args){
         String s = "Dupa = ";
         int wymiar = 34;
         System.out.println(s + wymiar);

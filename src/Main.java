43f7a62837c73827a15c48dfb245056f8e28be79
Artur Ciesla <ezechiasz.ofm@gmail.com>
2018-12-08 21:05:31 +0100

    0.04

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
-        String s = "Dupa = ";
-        int w1, w2, w3;
-        int wymiar = 34;
-        w1 = wymiar *2;
-        w1++;
-        System.out.println(s + wymiar+ "  "+ w1);
+       System.out.println();
+       System.out.print("Do ilu szukać? : ");
+
+       Scanner odczyt = new Scanner(System.in);
+       long liczba = odczyt.nextLong();
+
+
+
+
     }
 }

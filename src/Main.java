0a7919e8ed62b03d974614fed5dcc9ea595e01e0
Artur Ciesla <ezechiasz.ofm@gmail.com>
2018-12-08 21:34:27 +0100

    0.06

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,22 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-       System.out.println();
-       System.out.print("Do ilu szukać? : ");
+        System.out.println();
+        System.out.print("Do ilu szukać? : ");
 
-       Scanner odczyt = new Scanner(System.in);
-       long ile = odczyt.nextLong();
-       System.out.println(1);
+        Scanner odczyt = new Scanner(System.in);
+        long ile = odczyt.nextLong();
+        System.out.println(1);
 
-       for (long liczba=2;liczba<=ile;liczba++ ) {
-           boolean pierwsza = true;
-           for (long i = 2; i < liczba; i++)
-               if (liczba%i == 0) {
-                   pierwsza = false;
-                   i = liczba-1;
-               }
-           if (pierwsza) System.out.println(liczba);
-       }
+        for (long liczba=2;liczba<=ile;liczba++ ) {
+            boolean pierwsza = true;
+            for (long i = 2; i < liczba; i++)
+                if (liczba%i == 0) {
+                    pierwsza = false;
+                    i = liczba-1;
+                }
+            if (pierwsza) System.out.print(" " + liczba);
+        }
 
 
     }

465c11874d38c5f66ea135f0982043a1f122574c
Artur Ciesla <ezechiasz.ofm@gmail.com>
2018-12-08 21:25:35 +0100

    0.05

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,18 @@ public class Main {
        System.out.print("Do ilu szukać? : ");
 
        Scanner odczyt = new Scanner(System.in);
-       long liczba = odczyt.nextLong();
+       long ile = odczyt.nextLong();
+       System.out.println(1);
 
-
+       for (long liczba=2;liczba<=ile;liczba++ ) {
+           boolean pierwsza = true;
+           for (long i = 2; i < liczba; i++)
+               if (liczba%i == 0) {
+                   pierwsza = false;
+                   i = liczba-1;
+               }
+           if (pierwsza) System.out.println(liczba);
+       }
 
 
     }
